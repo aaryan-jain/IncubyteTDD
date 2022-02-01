a4266e139cd1a3c809f22d408f1380ce67f9493b
krazy-coder <aaryan.vijayraj@gmail.com>
2022-02-01 20:54:00 +0530

    Created a simple calculator with specified method signature

--- src/com/aaryan/crafting/Calculator.java
+++ src/com/aaryan/crafting/Calculator.java
@@ -1,5 +1,21 @@
 package com.aaryan.crafting;
 
 public class Calculator {
-
+		public int Add(String numbers) {
+			
+			int sum = 0;
+			
+			if(!numbers.isEmpty()) {			
+				String arr[] = numbers.split(",");
+				//int arr1[] = new int[arr.length];
+				for(int i = 0; i < arr.length; i++) {
+					sum = sum + Integer.parseInt(arr[i]);
+				}
+			}
+			
+			return sum;
+			
+		}
+		
+	
 }
