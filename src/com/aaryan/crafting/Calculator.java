707fd2a9f088c601e9d5649481f29295ddf5f250
krazy-coder <aaryan.vijayraj@gmail.com>
2022-02-02 21:07:51 +0530

    found out a bug (just after speaking with mansi about where to share the repo link) fixed it, refined the code, added final test cases and Voila!!

--- src/com/aaryan/crafting/Calculator.java
+++ src/com/aaryan/crafting/Calculator.java
@@ -3,43 +3,43 @@ package com.aaryan.crafting;
 import java.util.ArrayList;
 
 public class Calculator {
-		public int Add(String numbers) throws Exception {
-			
-			int sum = 0;
-			
-			if(!numbers.isEmpty()) {			
-				String arr[] = numbers.split("\n");
-//				int x = 0;
-//				for(String s : arr) System.out.println(++x + " " + s);
-//				System.out.println(arr.length);
-				ArrayList <Integer> arr1 = new ArrayList<>();
-				
-				String delim = ",";
-				
-				if(arr[0].contains("//")) delim = arr[0].split("//")[1];
-				
-				for(int i = 0; i < arr.length; i++) {
-					String temp[] = arr[i].split(delim);
-					for(int j =0; j< temp.length; j++) {
-						if(Integer.parseInt(temp[j]) < 0) {
-							throw new Exception("negatives not allowed " + temp[j]);
-						} else {
-							arr1.add(Integer.parseInt(temp[j]));
-						}
+	public int Add(String numbers) throws Exception {
+
+		int sum = 0;
+
+		if (!numbers.isEmpty()) {
+			String arr[] = numbers.split("\n");
+
+			ArrayList<Integer> arr1 = new ArrayList<>();
+
+			String delim = ",";
+			int x = 0;
+
+			if (arr[0].contains("//")) {
+				delim = "" + arr[0].charAt(2);
+				x = 1;
+			}
+
+			for (int i = x; i < arr.length; i++) {
+				String temp[] = arr[i].split(delim);
+				for (int j = 0; j < temp.length; j++) {
+					if (temp[j].isEmpty())
+						throw new Exception("negatives not allowed " + temp[j]);
+					if (Integer.parseInt(temp[j]) < 0) {
+						throw new Exception("negatives not allowed " + temp[j]);
+					} else {
+						arr1.add(Integer.parseInt(temp[j]));
 					}
 				}
-				
-				
-				for(int i = 0; i < arr1.size(); i++) {
-					sum = sum + arr1.get(i);
-//					System.out.println(arr1.get(i) + " " + sum);
-				}
 			}
-			
-			return sum;
-			
+
+			for (int i = 0; i < arr1.size(); i++) {
+				sum = sum + arr1.get(i);
+			}
 		}
-		
-	
-		
+
+		return sum;
+
+	}
+
 }
--- src/com/aaryan/crafting/CalculatorTests.java
+++ src/com/aaryan/crafting/CalculatorTests.java
@@ -6,50 +6,69 @@ import org.junit.jupiter.api.Test;
 
 class CalculatorTests {
 	Calculator c = new Calculator();
-	
-	
-		
+
 	@Test
 	void testThatNeedsToBePassed() {
 		try {
-		assertEquals(0,c.Add(""), "Test passed");
-		assertEquals(5, c.Add("0,5"));
-		assertEquals(7, c.Add("7,0"));
-		assertEquals(13, c.Add("6,7"));
-		assertEquals(10, c.Add("1,2,3,4"));
-	} catch(Exception e) {
-		System.out.println(e);
-	}
+			assertEquals(0, c.Add(""), "Test passed");
+			assertEquals(5, c.Add("0,5"));
+			assertEquals(7, c.Add("7,0"));
+			assertEquals(13, c.Add("6,7"));
+			assertEquals(10, c.Add("1\n2,3,4"));
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 
 	}
-	
-	
+
 	@Test
 	void testThatNeedsToBePassed2() {
 		try {
+			assertEquals(4, c.Add("4"));
+			assertEquals("negatives not allowed -1", c.Add("-1"));
+			// assertEquals(0, c.Add("5,4,-9"));
 
-		assertEquals("negatives not allowed -1", c.Add("-1"));
-		//assertEquals(0, c.Add("5,4,-9"));
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 
-	} catch(Exception e) {
-		System.out.println(e);
 	}
 
-	}
-	
-	
 	@Test
 	void testThatNeedsToBePassed3() {
 		try {
 
-		//assertEquals("negatives not allowed -1", c.Add("-1"));
-		assertEquals(0, c.Add("5,4,-9"));
+			// assertEquals("negatives not allowed -1", c.Add("-1"));
+			assertEquals(0, c.Add("5,4,-9"));
 
-	} catch(Exception e) {
-		System.out.println(e);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
 	}
+
+	@Test
+	void testThatNeedsToBePassed4() {
+		try {
+
+			assertEquals(3, c.Add("//;\n1;2"));
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 
 	}
 	
-	
+	@Test
+	void testThatNeedsToBePassed5() {
+		try {
+
+			assertEquals("negatives not allowed ", c.Add("//-\n1--2"));
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+	}
+
 }
