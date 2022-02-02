654304024e30e21e35a984adb1a5db89d2088c6a
krazy-coder <aaryan.vijayraj@gmail.com>
2022-02-02 15:23:05 +0530

    refined code and added more test cases

--- src/com/aaryan/crafting/Calculator.java
+++ src/com/aaryan/crafting/Calculator.java
@@ -1,15 +1,38 @@
 package com.aaryan.crafting;
 
+import java.util.ArrayList;
+
 public class Calculator {
-		public int Add(String numbers) {
+		public int Add(String numbers) throws Exception {
 			
 			int sum = 0;
 			
 			if(!numbers.isEmpty()) {			
-				String arr[] = numbers.split(",");
-				//int arr1[] = new int[arr.length];
+				String arr[] = numbers.split("\n");
+//				int x = 0;
+//				for(String s : arr) System.out.println(++x + " " + s);
+//				System.out.println(arr.length);
+				ArrayList <Integer> arr1 = new ArrayList<>();
+				
+				String delim = ",";
+				
+				if(arr[0].contains("//")) delim = arr[0].split("//")[1];
+				
 				for(int i = 0; i < arr.length; i++) {
-					sum = sum + Integer.parseInt(arr[i]);
+					String temp[] = arr[i].split(delim);
+					for(int j =0; j< temp.length; j++) {
+						if(Integer.parseInt(temp[j]) < 0) {
+							throw new Exception("negatives not allowed " + temp[j]);
+						} else {
+							arr1.add(Integer.parseInt(temp[j]));
+						}
+					}
+				}
+				
+				
+				for(int i = 0; i < arr1.size(); i++) {
+					sum = sum + arr1.get(i);
+//					System.out.println(arr1.get(i) + " " + sum);
 				}
 			}
 			
@@ -18,4 +41,5 @@ public class Calculator {
 		}
 		
 	
+		
 }
--- src/com/aaryan/crafting/CalculatorTests.java
+++ src/com/aaryan/crafting/CalculatorTests.java
@@ -7,12 +7,49 @@ import org.junit.jupiter.api.Test;
 class CalculatorTests {
 	Calculator c = new Calculator();
 	
+	
+		
 	@Test
 	void testThatNeedsToBePassed() {
+		try {
 		assertEquals(0,c.Add(""), "Test passed");
 		assertEquals(5, c.Add("0,5"));
-		assertEquals(5, c.Add("5,0"));
+		assertEquals(7, c.Add("7,0"));
 		assertEquals(13, c.Add("6,7"));
+		assertEquals(10, c.Add("1,2,3,4"));
+	} catch(Exception e) {
+		System.out.println(e);
 	}
 
+	}
+	
+	
+	@Test
+	void testThatNeedsToBePassed2() {
+		try {
+
+		assertEquals("negatives not allowed -1", c.Add("-1"));
+		//assertEquals(0, c.Add("5,4,-9"));
+
+	} catch(Exception e) {
+		System.out.println(e);
+	}
+
+	}
+	
+	
+	@Test
+	void testThatNeedsToBePassed3() {
+		try {
+
+		//assertEquals("negatives not allowed -1", c.Add("-1"));
+		assertEquals(0, c.Add("5,4,-9"));
+
+	} catch(Exception e) {
+		System.out.println(e);
+	}
+
+	}
+	
+	
 }
